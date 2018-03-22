7dc2a9f1bbd9303d21527257be1fc73d50c00b0b
jetong <jose.e.tong@gmail.com>
2018-03-22 18:44:26 +0000

    fix: argument to parseInt() was always 'income' fix: variable declarations moved out of function update()

--- store.js
+++ store.js
@@ -1,21 +1,28 @@
-var income = sessionStorage.getItem("income") || 0;
-var housing = sessionStorage.getItem("housing") || 0;
-var transportation = sessionStorage.getItem("transportation") || 0;
-var education = sessionStorage.getItem("education") || 0;
-var personal = sessionStorage.getItem("personal") || 0;
-var savings = sessionStorage.getItem("savings") || 0;
+var income, 
+    housing, 
+    transportation, 
+    education, 
+    personal, 
+    savings;
 
 function update() {
+  income = sessionStorage.getItem("income") || 0;
+  housing = sessionStorage.getItem("housing") || 0;
+  transportation = sessionStorage.getItem("transportation") || 0;
+  education = sessionStorage.getItem("education") || 0;
+  personal = sessionStorage.getItem("personal") || 0;
+  savings = sessionStorage.getItem("savings") || 0;
+
   sessionStorage.setItem("income", 
     parseInt(income) + parseInt(document.getElementById("income").value));
   sessionStorage.setItem("housing", 
-    parseInt(income) + parseInt(document.getElementById("housing").value));
+    parseInt(housing) + parseInt(document.getElementById("housing").value));
   sessionStorage.setItem("transportation", 
-    parseInt(income) + parseInt(document.getElementById("transportation").value));
+    parseInt(transportation) + parseInt(document.getElementById("transportation").value));
   sessionStorage.setItem("education", 
-    parseInt(income) + parseInt(document.getElementById("education").value));
+    parseInt(education) + parseInt(document.getElementById("education").value));
   sessionStorage.setItem("personal", 
-    parseInt(income) + parseInt(document.getElementById("personal").value));
+    parseInt(personal) + parseInt(document.getElementById("personal").value));
   sessionStorage.setItem("savings", 
-    parseInt(income) + parseInt(document.getElementById("savings").value));
+    parseInt(savings) + parseInt(document.getElementById("savings").value));
 }
